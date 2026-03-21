0f395b1839fc1cf2884e7b35deae6e39302fcad6
antoniovga1965-glitch <antoniovga1965@gmail.com>
2026-03-21 17:46:52 +0300

    use REDIS_URL for connection

--- bullmq/connection.js
+++ bullmq/connection.js
@@ -4,11 +4,17 @@
 // };
 // module.exports = reddisconnection;
 
-const reddisconnection = {
-  port: process.env.REDISPORT || 6379,
-  host: process.env.REDISHOST || 'localhost',
-  password: process.env.REDISPASSWORD || undefined,
-  username: process.env.REDISUSER || 'default',
-};
+// const reddisconnection = {
+//   port: process.env.REDISPORT || 6379,
+//   host: process.env.REDISHOST || 'localhost',
+//   password: process.env.REDISPASSWORD || undefined,
+//   username: process.env.REDISUSER || 'default',
+// };
+
+// module.exports = reddisconnection;
+
+const Redis = require('ioredis');
+
+const reddisconnection = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 
 module.exports = reddisconnection;
