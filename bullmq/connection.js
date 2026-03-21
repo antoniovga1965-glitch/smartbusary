87d9856d04c3cbccc363a465c592ef8e2b89c701
antoniovga1965-glitch <antoniovga1965@gmail.com>
2026-03-21 17:30:26 +0300

    fix redis connection for railway

--- bullmq/connection.js
+++ bullmq/connection.js
@@ -1,5 +1,14 @@
+// const reddisconnection = {
+//   port:6379,
+//   host:'localhost',
+// };
+// module.exports = reddisconnection;
+
 const reddisconnection = {
-  port:6379,
-  host:'localhost',
+  port: process.env.REDISPORT || 6379,
+  host: process.env.REDISHOST || 'localhost',
+  password: process.env.REDISPASSWORD || undefined,
+  username: process.env.REDISUSER || 'default',
 };
+
 module.exports = reddisconnection;
